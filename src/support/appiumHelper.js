85236957fa873e0515a334d6404b5f6b17d3eb03
Julio <julio958214@gmail.com>
2023-09-05 18:46:56 -0300

    code refactory

--- src/support/appiumHelper.js
+++ src/support/appiumHelper.js
@@ -1,8 +1,8 @@
-const Helper = require("@codeceptjs/helper");
+const Helper = require('@codeceptjs/helper');
 
 class AppiumHelper extends Helper {
     getScreenSize(){
-        const browser = this.helpers["Appium"].browser;
+        const browser = this.helpers['Appium'].browser;
         return browser.capabilities.viewportRect;
     }
 }
