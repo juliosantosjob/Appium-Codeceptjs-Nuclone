4832dcd243ba7cc1a7b2ea356dd7dfa06dc974de
juliosantosjob <juliosantosjob@users.noreply.github.com>
2023-11-27 16:44:10 +0000

    Automated deployment: Mon Nov 27 16:44:10 UTC 2023 432635ddf1c7513665cd5d291c81251d86ab2bdb

--- pages.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    usability: require('./src/pages/usability.pages.js'),
-    app: require('./src/pages/app.pages.js'),
-    search: require('./src/pages/searchCotact.pages')
-}
