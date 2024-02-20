d13a5a6d300bffca8c81b2534c2a5e82ba9085ab
Julio Santos <julioc.9730@gmail.com>
2024-02-19 22:44:59 -0300

    Atualizar o home.pages.js

--- src/pages/home.pages.js
+++ src/pages/home.pages.js
@@ -30,12 +30,12 @@ module.exports = {
         I.waitForElement(this.fields.txtDemand);
         I.waitForElement(this.fields.txtDeposit);
         
-        await I.doASwipe({ from: this.fields.txtDeposit, direction: "screenLeft"});
+        await I.doASwipe({ from: this.fields.txtDeposit, direction: 'screenLeft' });
 
         I.waitForElement(this.fields.txtTransfer);
         I.waitForElement(this.fields.txtPay);
 
-        await I.doASwipe({ from: this.fields.txtPay, direction: "screenLeft"});
+        await I.doASwipe({ from: this.fields.txtPay, direction: 'screenLeft' });
 
         I.waitForElement(this.fields.txtBlockCard);
         I.waitForElement(this.fields.txtVirtualCard);
